6d7cb55ebe9d3a98eba4e2bc7aac1a9b2174ffb0
kipngenohaaron <kipngenohaaron@gmail.com>
2023-05-25 21:55:30 +0300

    background color modified,,

--- java script/script.js
+++ java script/script.js
@@ -1,30 +1,19 @@
 
-        // JavaScript code goes here
-
-        // Example: Changing the background color after 2 seconds
         setTimeout(function() {
             document.body.style.backgroundColor = '#f0d3a9';
         }, 2000);
    // Wait for the page to fully load
    window.addEventListener('load', function() {
-    // Set a timeout to change the background color after 2 seconds
+   
     setTimeout(function() {
         document.body.style.backgroundColor = 'white';
     }, 2000);
 });
-// Wait for the page to fully load
+
 window.addEventListener('load', function() {
-    // Set a timeout to change the background color after 2 seconds
     setTimeout(function() {
-        document.body.style.backgroundColor = 'red';
-        document.body.style.backgroundImage = 'linear-gradient(to bottom, white, white)';
+        document.body.style.backgroundColor = 'green';
+        document.body.style.backgroundImage = 'linear-gradient(to bottom, lightgreen, white)';
     }, 2000);
 });
-    // // Wait for the page to fully load
-    // window.addEventListener('load', function() {
-    //     // Set a timeout to change the background color after 2 seconds
-    //     setTimeout(function() {
-    //         document.body.style.backgroundColor = 'white';
-    //         document.body.style.backgroundImage = 'linear-gradient(to bottom, navy, grey)';
-    //     }, 2000);
-    // });
+    

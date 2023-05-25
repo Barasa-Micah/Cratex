0bb1e723fb721ffab4912928a2f1c37bebbc9b76
kipngenohaaron <kipngenohaaron@gmail.com>
2023-05-25 21:26:53 +0300

    background colorflipping

--- java script/script.js
+++ java script/script.js
@@ -5,4 +5,18 @@
         setTimeout(function() {
             document.body.style.backgroundColor = '#f0d3a9';
         }, 2000);
-   
+   // Wait for the page to fully load
+   window.addEventListener('load', function() {
+    // Set a timeout to change the background color after 2 seconds
+    setTimeout(function() {
+        document.body.style.backgroundColor = 'green';
+    }, 2000);
+});
+// Wait for the page to fully load
+window.addEventListener('load', function() {
+    // Set a timeout to change the background color after 2 seconds
+    setTimeout(function() {
+        document.body.style.backgroundColor = 'red';
+        document.body.style.backgroundImage = 'linear-gradient(to bottom, navy, navy)';
+    }, 2000);
+});

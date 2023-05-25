2b28c923694a144e406cb937ebd8dd552f1fde5d
kipngenohaaron <kipngenohaaron@gmail.com>
2023-05-25 16:33:44 +0300

    script.js linked

---
+++ java script/script.js
@@ -0,0 +1,8 @@
+
+        // JavaScript code goes here
+
+        // Example: Changing the background color after 2 seconds
+        setTimeout(function() {
+            document.body.style.backgroundColor = '#f0d3a9';
+        }, 2000);
+   
